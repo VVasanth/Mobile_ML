41ed542b5b66ab775b21e8b62cbf820fc4e61f59
V Vasanth <vvk.victory@gmail.com>
2020-06-16 23:38:37 -0500

    reactNative files for base Mobile App

---
+++ App.js
@@ -0,0 +1,121 @@
+import React, {useEffect} from 'react';
+import { Button, View, Text, ActivityIndicator } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import MainTabScreen from './screens/MainTabScreen';
+import SupportScreen from './screens/SupportScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import BookmarkScreen from './screens/BookmarkScreen';
+
+import {DrawerContent} from './screens/DrawerContent';
+import RootStackScreen from './screens/RootStackScreen';
+
+import {AuthContext} from './components/context';
+
+const Drawer = createDrawerNavigator();
+
+
+function App() {
+
+  //const [isLoading, setIsLoading] = React.useState(true);
+  //const [userToken, setUserToken] = React.useState(null);
+
+  const initialLoginState = {
+    isLoading: true,
+    userName: null,
+    userToken:null,
+  }
+
+  const loginReducer = (prevState, action) => {
+    switch(action.type) {
+      case 'RETRIEVE_TOKEN':
+        return {
+          ...prevState,
+          userToken: action.token,
+          isLoading: false,          
+        }
+      case 'LOGIN':
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,          
+        }
+      case 'LOGOUT':
+        return {
+          ...prevState,
+          userName:null,
+          userToken:null,
+          isLoading: false,          
+        }
+      case 'REGISTER':
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,          
+        }
+    }
+  }
+
+  const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
+
+
+  const authContext = React.useMemo(()=> ({
+    signIn:(userName, password)=>{
+      //setUserToken('gdah');
+      //setIsLoading(false);
+      let userToken;
+      userToken = null;
+      if( userName == 'user' && password == 'pass'){
+        userToken = 'dfgdfg';
+      }
+      dispatch ({type: 'LOGIN', id: userName, token: userToken})
+    },
+    signOut: () => {
+      //setUserToken(null);
+      //setIsLoading(false);
+      dispatch({type:'LOGOUT'})
+    },
+    signUp: () => {
+      setUserToken('gdah');
+      setIsLoading(false);
+    }
+  }), []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      //setIsLoading(false);
+      dispatch({type:'RETRIEVE_TOKEN', token:'dfklj'})
+    }, 1000);
+  }, []
+  );
+
+  if(loginState.isLoading){
+    return(
+      <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+        <ActivityIndicator size="large"/>
+        </View>
+    )
+  }
+
+  return (
+    <AuthContext.Provider value={authContext}>
+    <NavigationContainer>
+
+      { loginState.userToken != null ?
+      (
+        <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+        <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
+        <Drawer.Screen name="SupportScreen" component={SupportScreen} />
+        <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+        <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
+      </Drawer.Navigator> )
+      : <RootStackScreen/>}
+     </NavigationContainer>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
